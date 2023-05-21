4ed47f3ecd7fd2ca8da3035f9944bc9bd7c5768e
edmorillo <edisonmorillo489@gmail.com>
2023-05-21 17:00:24 -0300

    Agregar archivos de proyecto.

---
+++ Parcial/Controllers/FormularioController.cs
@@ -0,0 +1,49 @@
+﻿using Parcial.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Reflection;
+
+namespace Parcial.Controllers
+{
+    public class FormularioController : Controller
+    {
+
+        public static List<Formulario> formularioList = new List<Formulario>();
+        public IActionResult Index()
+        {
+
+            return View(formularioList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Formulario pag)
+        {
+            try
+            {
+                formularioList.Add(pag);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        /* [HttpPost]
+         public IActionResult Create(IDtributaria tarea)
+         {
+             tarea.Id = tributaria.Count + 1;
+             tarea.Id = tributaria.Count + 1;
+             tributaria.Add(tarea);
+             return RedirectToAction("Index");
+         }*/
+    }
+}
---
+++ Parcial/Models/Formulario.cs
@@ -0,0 +1,15 @@
+﻿namespace Parcial.Models
+{
+    public class Formulario
+    {
+        public string Condicion_Iva { get; set; }
+        
+
+        public double Cuit { get; set; }
+
+        public string Razon_Social { get; set; }
+        public string Domicilio { get; set; }
+        public string Email { get; set; }
+        public string Condicion_Venta { get; set; }
+    }
+}
